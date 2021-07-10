1e832db449a48aa1fa2ee49fa8d5986c1799c9db
Stacey McKnight <mcknight.stm@gmail.com>
2021-07-10 18:32:31 -0500

    add orders routes and format customer routes to match syntax

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   #namespace api with versioning
   namespace :api do
     namespace :v1 do
-      resources :customers, except: [:show, :new]
+      resources :customers, only: [:index, :create, :destroy]
+      resources :orders, only: [:index, :create, :destroy]
     end
   end
 end
