277bc553cf4cf983e3523afc9573271e0d1cdff5
Stacey McKnight <mcknight.stm@gmail.com>
2021-07-10 17:34:37 -0500

    remove comma in customers migration

--- db/migrate/20210710222917_create_customers.rb
+++ db/migrate/20210710222917_create_customers.rb
@@ -3,7 +3,7 @@ class CreateCustomers < ActiveRecord::Migration[6.0]
     create_table :customers do |t|
       t.string :first_name
       t.string :last_name
-      t.integer, :age
+      t.integer :age
       t.integer :phone_number
 
       t.timestamps
