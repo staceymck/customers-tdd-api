bdd15bc326e518addb157c10c9b41c0e2f7a4cba
Stacey McKnight <mcknight.stm@gmail.com>
2021-07-10 17:44:55 -0500

    add tests for order model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #Association
+  it { should belong_to(:customer) }
+  #Validations
+  it { should validate_presence_of(:item_name) }
 end
