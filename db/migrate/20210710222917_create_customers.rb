8de35fe4bae6b684211f1ee6114a90a6d43f01a9
Stacey McKnight <mcknight.stm@gmail.com>
2021-07-10 17:29:56 -0500

    run customers model generator

---
+++ db/migrate/20210710222917_create_customers.rb
@@ -0,0 +1,12 @@
+class CreateCustomers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :customers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer, :age
+      t.integer :phone_number
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/customers.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :customer do
+    first_name { "MyString" }
+    last_name { "MyString" }
+    age { "" }
+    phone_number { 1 }
+  end
+end
