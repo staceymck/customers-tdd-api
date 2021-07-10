65e07536685bf9d5fac03ac0f29caf5315ffeb36
Stacey McKnight <mcknight.stm@gmail.com>
2021-07-10 17:42:37 -0500

    add tests for customer model

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
+#Concise format possible because of ShouldaMatcher gem
+
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #Associations
+  it { should have_many(:orders) }
+  #Validations
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:age) }
+  it { should validate_presence_of(:phone_number) }
 end
