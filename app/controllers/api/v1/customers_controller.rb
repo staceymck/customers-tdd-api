4f3b9f5b5b702d9df2334d084c68b49bffefcc04
Stacey McKnight <mcknight.stm@gmail.com>
2021-07-10 18:37:23 -0500

    build out controllers for customers and orders

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,2 +1,27 @@
 class Api::V1::CustomersController < ApplicationController
+
+  def index
+    @customers = Customer.all
+    render json: @customers
+  end
+
+  def create
+    @customer = Customer.create(customer_params)
+    if @customer.valid?
+      render json: @customer, status: 201 #Created
+    else
+      render json: @customer.errors, status: 422 #Unprocessable Entity
+    end
+  end
+
+  def destroy
+    @customer = Customer.find(params[:id])
+    @customer.destroy
+    render status: 204 #No Content, but successful request
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :age, :phone_number)
+  end
 end
--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,2 +1,27 @@
 class Api::V1::OrdersController < ApplicationController
+
+  def index
+    @orders = Order.all
+    render json: @orders
+  end
+
+  def create
+    @order = Order.create(order_params)
+    if @order.valid?
+      render json: @order, status: 201
+    else
+      render json: @order.errors, status: 422
+    end
+  end
+
+  def destroy
+    @order = Order.find(params[:id])
+    @order.destroy
+    render status: 204
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:item_name, :customer_id)
+  end
 end
